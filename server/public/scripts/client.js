683e02c6a3acb9957438ab538c0920603597415f
Kris Szafranski <kdszafranski@gmail.com>
2017-07-05 09:55:28 -0500

    working ajax cycle

---
+++ server/public/scripts/client.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  console.log('jquery ready!');
+
+  // process input form
+  $("#inputForm").on("submit", function(e) {
+    e.preventDefault();
+    // console.log('submit event: ', e);
+
+    var question = {};
+    question.inputX = $("#inputX").val();
+    question.inputY = $("#inputY").val();
+    question.operation = $("#operation").val();
+    console.log("sending question to server: ", question);
+
+    $.ajax({
+      url: "/math",
+      type: "POST",
+      data: question,
+      success: function(response) {
+        console.log('ajax response: ', response);
+      }
+    });
+
+  });
+
+
+
+});
--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ var app = express();
 var port = process.env.PORT || 5000;
 var path = require('path');
 
+// routes
+app.post("/math", function(req, res) {
+  console.log('post to slash math and stuff');
+  res.send("we got it.");
+});
 
 
+// server back static files
 app.get("/*", function(req, res) {
   var file = req.params[0] || "/views/index.html";
   res.sendFile(path.join(__dirname, "/public/", file));
